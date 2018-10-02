05257b2d3803af7149e11e1e8ed698a02c1877cf
fkerrin <fkerrin@gmail.com>
2018-10-02 21:54:15 +0100

    Updated Lambda following troubleshooting

--- lambda_handler.py
+++ lambda_handler.py
@@ -11,30 +11,34 @@ def lambda_handler(event, context):
     BucketName = event['Records'][0]['s3']['bucket']['name']
     NewFileUploaded = event['Records'][0]['s3']['object']['key']
     
-    S3 = boto3.resource('s3', config = Config(signature_version = 's3v4'))  # Create an S3 resource - need this to refer to S3 objects
-    BuildBucket = S3.Bucket(BucketName)  # Where packaged code is stored
-    ZipObject = S3.Object(BucketName, NewFileUploaded)
-    PortfolioBucket = S3.Bucket('fkerrin.com')  # Where code will be unpackaged to
+    try:
+        S3 = boto3.resource('s3', config = Config(signature_version = 's3v4'))  # Create an S3 resource - need this to refer to S3 objects
+        BuildBucket = S3.Bucket(BucketName)  # Where packaged code is stored
+        ZipObject = S3.Object(BucketName, NewFileUploaded)
+        PortfolioBucket = S3.Bucket('fkerrin.com')  # Where code will be unpackaged to
 
-    # Create a pointer to the file object in memory and read the zipfile into that memory location
-    ZippedCode = io.BytesIO()
-    BuildBucket.download_fileobj(NewFileUploaded, ZippedCode)
-    
-    # Now unzip the contents of each file and send to the destination bucket
-    with zipfile.ZipFile(ZippedCode) as MyZip:
-        for FileName in MyZip.namelist():
-            UnzippedObject = MyZip.open(FileName)
-            PortfolioBucket.upload_fileobj(UnzippedObject, FileName,
-                ExtraArgs = {'ContentType' : mimetypes.guess_type(FileName)[0]})
-            PortfolioBucket.Object(FileName).Acl().put(ACL = 'public-read')
-            # ACL is required to give public permissions to the file objects
+        # Create a pointer to the file object in memory and read the zipfile into that memory location
+        ZippedCode = io.BytesIO()
+        BuildBucket.download_fileobj(NewFileUploaded, ZippedCode)
 
-    # Now need to notify that new code has been deployed
-    SNS = boto3.resource('sns')
-    Topic = SNS.Topic('arn:aws:sns:eu-west-1:258508589804:CodeDeployed')
-    NotificationMessage = 'New file {} received on Bucket {} and deployed to Bucket {}'.format(NewFileUploaded, BucketName, PortfolioBucket.name)
-    NotificationSubject = 'Notification for deployment of code on AWS S3'
-    Topic.publish(Message = NotificationMessage, Subject = NotificationSubject)
+        # Now unzip the contents of each file and send to the destination bucket
+        with zipfile.ZipFile(ZippedCode) as MyZip:
+            for FileName in MyZip.namelist():
+                UnzippedObject = MyZip.open(FileName)
+                PortfolioBucket.upload_fileobj(UnzippedObject, FileName,
+                    ExtraArgs = {'ContentType' : mimetypes.guess_type(FileName)[0]})
+                PortfolioBucket.Object(FileName).Acl().put(ACL = 'public-read')
+                # ACL is required to give public permissions to the file objects
+
+        # Now need to notify that new code has been deployed
+        SNS = boto3.resource('sns')
+        Topic = SNS.Topic('arn:aws:sns:eu-west-1:258508589804:CodeDeployed')
+        NotificationMessage = 'New file {} received on Bucket {} and deployed to Bucket {}'.format(NewFileUploaded, BucketName, PortfolioBucket.name)
+        NotificationSubject = 'Notification for deployment of code on AWS S3'
+        Topic.publish(Message = NotificationMessage, Subject = NotificationSubject)
+    except:
+        NotificationMessage = 'Deployent of code was unsuccessful - please check configurations'
+        NotificationSubject = 'Code Deployment Failed'
+        Topic.publish(Message = NotificationMessage, Subject = NotificationSubject)
 
     return
-
