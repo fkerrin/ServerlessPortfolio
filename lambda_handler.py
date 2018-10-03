c7edf90b671fd4d0610aa1976b05d719ecb8494e
fkerrin <fkerrin@gmail.com>
2018-10-03 18:42:28 +0100

    Testing CodePupeline

--- lambda_handler.py
+++ lambda_handler.py
@@ -6,19 +6,27 @@ from botocore.client import Config  # To change the config of the S3 resource to
 
 def lambda_handler(event, context):
 
-    # Event trigger is new file on S3 - get the bucket name and the new file from the event recordset
-    # Assuming only one event so there should only be one record - ['records'][0]
-    BucketName = event['Records'][0]['s3']['bucket']['name']
-    NewFileUploaded = event['Records'][0]['s3']['object']['key']
-    
-    S3 = boto3.resource('s3', config = Config(signature_version = 's3v4'))  # Create an S3 resource - need this to refer to S3 objects
-    BuildBucket = S3.Bucket(BucketName)  # Where packaged code is stored
-    ZipObject = S3.Object(BucketName, NewFileUploaded)
-    PortfolioBucket = S3.Bucket('fkerrin.com')  # Where code will be unpackaged to
-    SNS = boto3.resource('sns')
-    Topic = SNS.Topic('arn:aws:sns:eu-west-1:258508589804:CodeDeployed')
+    """ Event trigger can be CodePipelne trigger or new file on S3 - get the bucket name and the new file from the event recordset.
+     Assuming only one event in each case - ['CodePipeline.job']['data']['inputArtifacts'][0] or ]['records'][0]"""
+    Job = event.get('CodePipeline.job')  # This will get the CodePipeline job if this was the trigger or None if not
+    if Job:
+        BucketName = Job['data']['inputArtifacts'][0]['location']['s3Location']['bucketName']
+        NeFileUploaded = Job['data']['inputArtifacts'][0]['location']['s3Location']['objectKey']
+    elif:
+        BucketName = event['Records'][0]['s3']['bucket']['name']
+        NewFileUploaded = event['Records'][0]['s3']['object']['key']
+    else:
+        BucketName = NewFileUploaded = ''  # This will cause exception and generate message below
+    print('New file {} was uploaded to S3 bucket {}'.format(NewFileUploaded, BucketName))
 
     try:
+        S3 = boto3.resource('s3', config = Config(signature_version = 's3v4'))  # Create an S3 resource - need this to refer to S3 objects
+        BuildBucket = S3.Bucket(BucketName)  # Where packaged code is stored
+        ZipObject = S3.Object(BucketName, NewFileUploaded)
+        PortfolioBucket = S3.Bucket('fkerrin.com')  # Where code will be unpackaged to
+        SNS = boto3.resource('sns')
+        Topic = SNS.Topic('arn:aws:sns:eu-west-1:258508589804:CodeDeployed')
+
         # Create a pointer to the file object in memory and read the zipfile into that memory location
         ZippedCode = io.BytesIO()
         BuildBucket.download_fileobj(NewFileUploaded, ZippedCode)
@@ -32,13 +40,19 @@ def lambda_handler(event, context):
                 PortfolioBucket.Object(FileName).Acl().put(ACL = 'public-read')
                 # ACL is required to give public permissions to the file objects
 
-        # Now need to notify that new code has been deployed
+        # Now need to notify to SNS and CodePipeline that new code has been deployed
         NotificationMessage = 'New file {} received on Bucket {} and deployed to Bucket {}'.format(NewFileUploaded, BucketName, PortfolioBucket.name)
         NotificationSubject = 'Notification for deployment of code on AWS S3'
         Topic.publish(Message = NotificationMessage, Subject = NotificationSubject)
-    except:  # Send an error message to SNS if not successful
+        if Job:
+            CodePipeline = boto3.client('codepipeline')
+            CodePipeline.put_job_success_result(jobId = Job['id'])
+    except:
         NotificationMessage = 'Deployent of code was unsuccessful - please check configurations'
         NotificationSubject = 'Code Deployment Failed'
         Topic.publish(Message = NotificationMessage, Subject = NotificationSubject)
+        if Job:
+            CodePipeline = boto3.client('codepipeline')
+            CodePipeline.put_job_failure_result(jobId = Job['id'])
 
     return
